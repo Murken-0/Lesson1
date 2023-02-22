56a7a045dbf519840895dee5eeb531497f55bf91
Murken-0 <muravievvlad.2002@gmail.com>
2023-02-22 14:10:56 +0300

    add landscape orientation

--- app/src/main/java/ru/mirea/MuravievVR/lesson1/MainActivity.java
+++ app/src/main/java/ru/mirea/MuravievVR/lesson1/MainActivity.java
@@ -1,14 +1,20 @@
 package ru.mirea.MuravievVR.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+        EditText myText = (EditText) findViewById(R.id.editTextTextPersonName);
+        myText.setText("New text in MIREA");
+        Button button = findViewById(R.id.button13);
+        button.setText("MireaButton");
+        CheckBox check = findViewById(R.id.checkBox);
+        check.setChecked(true);
     }
 }
--- buttonclicker/src/main/java/ru/mirea/muravievvr/buttonclicker/MainActivity.java
+++ buttonclicker/src/main/java/ru/mirea/muravievvr/buttonclicker/MainActivity.java
@@ -1,8 +1,8 @@
-package ru.mirea.muravievvr.control_lesson1;
+package ru.mirea.muravievvr.buttonclicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
